0fab29e3d4e0c1ca7c6a841f79369afd51c7f7ba
Fernando Pereira Lisboa <fernandopereiralisboa@gmail.com>
2018-02-13 18:38:21 -0200

    Added listComprehensions

---
+++ python/listComprehensions.py
@@ -0,0 +1,16 @@
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+
+q = []
+
+for i in range(x + 1):
+  for j in range(y + 1):
+    for k in range(z + 1):
+      if ( ( i + j + k ) != n ):
+        q.append([i, j, k])
+
+
+print(q)
+
